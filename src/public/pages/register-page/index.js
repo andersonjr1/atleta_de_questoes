842832a9b0ee59e845323b73d13e4f98cab1ab37
Anderson <andersonpaulojr1@gmail.com>
2025-03-20 13:45:30 -0300

    refactor: delete unused files

--- src/public/pages/register-page/index.js
+++
@@ -1,7 +0,0 @@
-const formLogin = document.getElementById("formLogin");
-
-formLogin.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const formData = new FormData(formLogin);
-  console.log(formData);
-});
