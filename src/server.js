22dd272d1e7a305dca339d9c5833c81a15a8c4f0
Anderson <andersonpaulojr1@gmail.com>
2025-03-24 10:18:43 -0300

    feat: add error handling

--- src/server.js
+++ src/server.js
@@ -12,8 +12,18 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/login", express.static(path.join(__dirname, "public/pages/login")));
-app.use("/bem-vindo", express.static(path.join(__dirname, "public/pages/welcome")));
+app.use(
+  "/bem-vindo",
+  express.static(path.join(__dirname, "public/pages/welcome"))
+);
 
 app.use("/api", router);
+
+app.use((err, req, res, next) => {
+  res.status(400).json({
+    success: 0,
+    message: err.message || "Aconteceu algo de errado",
+  });
+});
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
