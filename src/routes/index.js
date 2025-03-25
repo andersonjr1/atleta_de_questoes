ad3efa8685059fea9aae17987283f9f3a0a5f1d4
Vinicius Tineli Paiva <vinicius.tinelipaiva@gmail.com>
2025-03-25 07:01:20 -0300

    chore: integrate question routes into main router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
+const questionRoutes = require("./questionRoutes.js");
 
 router.use("/", userRoutes);
+router.use("/", questionRoutes);
 
 module.exports = router;
