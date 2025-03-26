0bc1ff5ad49023420ffd3dc291f08b9b861cfc2f
Matheus Bispo Machtura <math.machtura@gmail.com>
2025-03-26 00:08:04 -0300

    feat: adds to question controller with delete action

--- src/controllers/questionController.js
+++ src/controllers/questionController.js
@@ -11,6 +11,18 @@ const questionController = {
             res.status(statusCode).json({ message: error.message });
         }
     },
+    delete: async (req, res) => {
+        try {
+            const { id } = req.params;
+
+            const response = await questionService.delete(id);
+
+            res.status(200).json(response);
+        } catch (error) {
+            const statusCode = error.status || 500;
+            res.status(statusCode).json({ message: error.message });
+        }
+    },
 };
 
 module.exports = { questionController };
