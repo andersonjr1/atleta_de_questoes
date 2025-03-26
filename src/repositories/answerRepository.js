155d0424c065ff4c88c6e32b40a1579791da2ea9
Vinicius Tineli Paiva <vinicius.tinelipaiva@gmail.com>
2025-03-26 05:08:48 -0300

    fest: add getUserAnsweredQuestions with joins and pagination

--- src/repositories/answerRepository.js
+++ src/repositories/answerRepository.js
@@ -53,14 +53,61 @@ const answerRepository = {
         try {
             const result = await pool.query(
                 `SELECT * FROM accounts_questions
-                WHERE id_account = $1 AND id_questio = $2`,
+                WHERE id_account = $1 AND id_question = $2`,
                 [accountId, questionId]
             );
             return result.rows[0];
         } catch (error) {
             throw error;
         }
-    }
+    },
+
+    getUserAnsweredQuestions: async (accountId, { page = 1, limit = 10, fromDate, toDate }) => {
+        try {
+            const offset = (page - 1) * limit;
+            let query = `
+            SELECT
+                aq.id,
+                q.id AS question_id,
+                q.title AS question_title,
+                q.context AS question_context,
+                qa.id AS alternative_id,
+                qa.text AS alternative_text,
+                qa.is_correct AS is_correct,
+                aq.exam AS is_exam,
+                aq.answered_at
+            FROM accounts_questions aq
+            JOIN questions q ON aq.id_question = q.id
+            JOIN question_alternatives qa ON aq.id_alternative = qa.id
+            WHERE aq.id_account = $1
+        `;
+
+            const params = [accountId];
+            let paramIndex = 2;
+
+            if (fromDate) {
+                query += `AND aq.answered_at >= $${paramIndex++}`;
+                params.push(new Date(fromDate));
+            }
+
+            if (toDate) {
+                query += ` AND aq.answered_at <= $${paramIndex++}`;
+                params.push(new Date(toDate));
+            }
+
+            query += `
+                ORDER BY aq.answered_at DESC
+                LIMIT $${paramIndex++} OFFSET $${paramIndex}
+            `;
+            params.push(limit, offset);
+
+            const result = await pool.query(query, params);
+            return result.rows;
+            
+        } catch (error) {
+            throw error;
+        }
+    },
 };
 
 module.exports = { answerRepository };
