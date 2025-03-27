ba81ab7a6d02b9cf9c756a347fec6682448d8a04
Anderson <andersonpaulojr1@gmail.com>
2025-03-27 10:53:52 -0300

    feat: add components
    - Added timer component.
    - Added headerWithMenu component.
    - Added headerWithoutMenu component.
    - Added questionExam component.
    - Added questionExamResult component.
    - Added all this components to exam.

--- src/public/pages/authorized-user/exam/exam.js
+++ src/public/pages/authorized-user/exam/exam.js
@@ -1,23 +1,38 @@
 const element = document.createElement("div");
+import Header from "/components/headerWithMenu.js";
+import HeaderWithoutMenu from "/components/headerWithoutMenu.js";
+import QuestionElement from "/components/questionExam.js";
+import QuestionElementResult from "/components/questionExamResult.js";
+import ConfirmationModal from "/components/confirmationModal.js";
+import Timer from "/components/timer.js";
 
 element.style.height = "100vh";
 element.style.display = "flex";
 element.style.flexDirection = "column";
 element.style.alignItems = "center";
+const nowDate = new Date();
+
+const simulado = {
+  id: "null",
+  startTime: new Date(nowDate),
+  endTime: new Date(nowDate.setMinutes(nowDate.getMinutes() + 35)),
+};
 
 function renderInitialPage() {
-  element.innerHTML = `
-    <div id="containerInitial">
+  const container = document.createElement("div");
+  container.id = "containerInitial";
+  container.innerHTML = `
         <main>
             <h1>SIMULADO ENEM</h1>
             <p>Um mini simulado do ENEM com 15 questões e duração aproximada de 35 minutos. O nível de dificuldade é X</p>
             <button id="startButton" class="button">INICIAR</button>
         </main>
         <img src="../../images/site/Simulado.png" id="examImage" alt="Estudante Ilustração">
-    </div>
     `;
 
-  const startButton = element.querySelector("#startButton");
+  const startButton = container.querySelector("#startButton");
+  element.appendChild(Header());
+  element.appendChild(container);
 
   startButton.addEventListener("click", renderQuestionsPage);
 }
@@ -112,20 +127,30 @@ function renderQuestionsPage() {
     },
   ];
 
-  element.innerHTML = `
-      <div id="containerExam">
-        <div id="containerQuestions"></div>
-        <button id="sendButton" class="button">RESPONDER</button>
-      </div>
-    `;
+  element.innerHTML = ``;
 
-  const containerQuestions = element.querySelector("#containerQuestions");
+  element.appendChild(HeaderWithoutMenu());
+
+  const containerExam = document.createElement("div");
+  containerExam.id = "containerExam";
+  element.appendChild(containerExam);
+
+  const timer = Timer(Date.now(), simulado.endTime.getTime(), renderResultPage);
+  containerExam.appendChild(timer);
+
+  const containerQuestions = document.createElement("div");
+  containerQuestions.id = "containerQuestions";
+  containerExam.appendChild(containerQuestions);
+
+  const sendButton = document.createElement("button");
+  sendButton.id = "sendButton";
+  sendButton.innerHTML = "RESPONDER";
+  sendButton.classList.add("button");
+  containerExam.appendChild(sendButton);
 
   questions.forEach((question, index) => {
-    containerQuestions.appendChild(createQuestionElement(question, index));
+    containerQuestions.appendChild(QuestionElement(question, index));
   });
-
-  const sendButton = element.querySelector("#sendButton");
 
   sendButton.addEventListener("click", () => {
     const allAnsered = questions.every((question) => {
@@ -133,16 +158,15 @@ function renderQuestionsPage() {
     });
 
     if (!allAnsered) {
-      const modalContainer = createConfirmationModal(
+      const modalContainer = ConfirmationModal(
         "Mandar respostas incompletas?",
         () => {
-          console.log("Enviado");
+          renderResultPage();
         }
       );
       element.appendChild(modalContainer);
       return;
     }
-    console.log("Enviado");
     renderResultPage();
   });
 }
@@ -255,426 +279,18 @@ function renderResultPage() {
       support_urls: ["https://www.youtube.com/watch?v=Xav6P3gSYdo"],
     },
   ];
+  element.innerHTML = "";
+  element.appendChild(Header());
 
-  element.innerHTML = `
-        <div id="containerResults">
-        </div>
-        `;
-
-  const containerResults = element.querySelector("#containerResults");
+  const containerResults = document.createElement("div");
+  containerResults.id = "containerResults";
+  element.appendChild(containerResults);
 
   questions.forEach((question, index) => {
-    containerResults.appendChild(createQuestionElementResult(question, index));
+    containerResults.appendChild(QuestionElementResult(question, index));
   });
 }
 
 renderInitialPage();
-
-function createQuestionElement(questionData, index) {
-  const questionContainer = document.createElement("div");
-  const questionInformation = document.createElement("div");
-  const questionContent = document.createElement("div");
-  questionContent.classList.add("question-container");
-  questionInformation.classList.add("question-information");
-  questionContainer.classList.add("question-container");
-  console.log(index);
-  questionInformation.innerHTML = `
-  <span>Questão ${index + 1}</span>
-  <span> - </span>
-  <span>Disciplina: ${questionData.discipline}</span>
-  <span class="spanStatus">+</span>
-  `;
-
-  questionInformation.style.position = "relative";
-  questionInformation.style.fontSize = "1.4rem";
-  questionInformation.style.padding = "0.6rem";
-  questionInformation.style.border = "1px solid black";
-
-  const spanStatus = questionInformation.querySelector(".spanStatus");
-
-  spanStatus.style.position = "absolute";
-  spanStatus.style.right = "10px";
-
-  questionContainer.appendChild(questionInformation);
-
-  questionContainer.style.width = "100%";
-  questionInformation.style.backgroundColor = "#BBBDEB";
-
-  questionInformation.addEventListener("click", () => {
-    if (questionContent.style.display === "none") {
-      questionContent.style.display = "block";
-      spanStatus.textContent = "-";
-    } else {
-      questionContent.style.display = "none";
-      spanStatus.textContent = "+";
-    }
-  });
-
-  questionContent.style.display = "none";
-  questionContent.style.padding = "0.6rem";
-  questionContent.style.borderTop = "none";
-  questionContent.style.borderBottom = "1px solid black";
-  questionContent.style.borderLeft = "1px solid black";
-  questionContent.style.borderRight = "1px solid black";
-
-  // Question Context
-  const contextDiv = document.createElement("div");
-  contextDiv.classList.add("question-context");
-  if (questionData.context) {
-    const contextParagraph = document.createElement("p");
-    contextParagraph.textContent = questionData.context;
-    contextDiv.appendChild(contextParagraph);
-  }
-
-  // Question Images
-  if (questionData.question_files && questionData.question_files.length > 0) {
-    questionData.question_files.forEach((fileUrl) => {
-      const img = document.createElement("img");
-      img.src = fileUrl;
-      img.alt = "Question Image";
-      img.style.maxWidth = "100%";
-      img.style.height = "auto";
-      contextDiv.appendChild(img);
-    });
-  }
-  questionContent.appendChild(contextDiv);
-
-  // Alternative Introduction
-  if (questionData.alternative_introduction) {
-    const introParagraph = document.createElement("p");
-    introParagraph.textContent = questionData.alternative_introduction;
-    questionContent.appendChild(introParagraph);
-  }
-
-  // Alternatives
-  const alternativesList = document.createElement("ul");
-  alternativesList.classList.add("alternatives-list");
-  alternativesList.style.listStyleType = "none";
-  alternativesList.style.padding = "0";
-  alternativesList.style.display = "flex";
-  alternativesList.style.flexDirection = "column";
-  alternativesList.style.gap = "10px";
-
-  questionData.alternatives.forEach((alternative, index, alternatives) => {
-    const listItem = document.createElement("li");
-    listItem.classList.add("alternative-item");
-
-    const label = document.createElement("label");
-    label.classList.add("alternative-label");
-
-    const radio = document.createElement("input");
-    radio.type = "radio";
-    radio.name = `question-${questionData.question_index}`;
-    radio.value = alternative.letter;
-    radio.id = `question-${questionData.question_index}-${alternative.letter}`;
-
-    label.appendChild(radio);
-
-    radio.addEventListener("change", () => {
-      alternatives.forEach((alt) => {
-        if (alt.letter === alternative.letter) {
-          alt.selected = true;
-        } else {
-          alt.selected = false;
-        }
-      });
-    });
-
-    const letterSpan = document.createElement("span");
-    letterSpan.textContent = `${alternative.letter}: `;
-    label.appendChild(letterSpan);
-
-    if (alternative.text) {
-      const textSpan = document.createElement("span");
-      textSpan.textContent = alternative.text;
-      label.appendChild(textSpan);
-    }
-
-    if (alternative.file) {
-      const img = document.createElement("img");
-      img.src = alternative.file;
-      img.alt = `Alternative ${alternative.letter} Image`;
-      img.style.maxWidth = "100%";
-      img.style.height = "auto";
-      label.appendChild(img);
-    }
-
-    listItem.appendChild(label);
-    alternativesList.appendChild(listItem);
-  });
-
-  questionContent.appendChild(alternativesList);
-  questionContainer.appendChild(questionContent);
-
-  return questionContainer;
-}
-
-function createQuestionElementResult(questionData, index) {
-  const questionContainer = document.createElement("div");
-  const questionInformation = document.createElement("div");
-  const questionContent = document.createElement("div");
-  questionContent.classList.add("question-container");
-  questionInformation.classList.add("question-information");
-  questionContainer.classList.add("question-container");
-  console.log(index);
-  questionInformation.innerHTML = `
-    <span>Questão ${index + 1}</span>
-    <span> - </span>
-    <span>Disciplina: ${questionData.discipline}</span>
-    <span class="spanStatus">+</span>
-    `;
-
-  questionInformation.style.position = "relative";
-  questionInformation.style.fontSize = "1.4rem";
-  questionInformation.style.padding = "0.6rem";
-  questionInformation.style.border = "1px solid black";
-
-  const spanStatus = questionInformation.querySelector(".spanStatus");
-
-  spanStatus.style.position = "absolute";
-  spanStatus.style.right = "10px";
-
-  questionContainer.appendChild(questionInformation);
-
-  questionContainer.style.width = "100%";
-  let rightAnswer = false;
-  questionData.alternatives.forEach((alternative) => {
-    if (alternative.is_correct && alternative.selected) {
-      rightAnswer = true;
-    }
-  });
-  questionInformation.style.backgroundColor = "#BBBDEB";
-
-  questionInformation.addEventListener("click", () => {
-    if (questionContent.style.display === "none") {
-      questionContent.style.display = "block";
-      spanStatus.textContent = "-";
-    } else {
-      questionContent.style.display = "none";
-      spanStatus.textContent = "+";
-    }
-  });
-
-  questionContent.style.display = "none";
-  questionContent.style.padding = "0.6rem";
-  questionContent.style.borderTop = "none";
-  questionContent.style.borderBottom = "1px solid black";
-  questionContent.style.borderLeft = "1px solid black";
-  questionContent.style.borderRight = "1px solid black";
-
-  // Question Context
-  const contextDiv = document.createElement("div");
-  contextDiv.classList.add("question-context");
-  if (questionData.context) {
-    const contextParagraph = document.createElement("p");
-    contextParagraph.textContent = questionData.context;
-    contextDiv.appendChild(contextParagraph);
-  }
-
-  // Question Images
-  if (questionData.question_files && questionData.question_files.length > 0) {
-    questionData.question_files.forEach((fileUrl) => {
-      const img = document.createElement("img");
-      img.src = fileUrl;
-      img.alt = "Question Image";
-      img.style.maxWidth = "100%";
-      img.style.height = "auto";
-      contextDiv.appendChild(img);
-    });
-  }
-  questionContent.appendChild(contextDiv);
-
-  // Alternative Introduction
-  if (questionData.alternative_introduction) {
-    const introParagraph = document.createElement("p");
-    introParagraph.textContent = questionData.alternative_introduction;
-    questionContent.appendChild(introParagraph);
-  }
-
-  // Alternatives
-  const alternativesList = document.createElement("ul");
-  alternativesList.classList.add("alternatives-list");
-  alternativesList.style.listStyleType = "none";
-  alternativesList.style.padding = "0";
-  alternativesList.style.display = "flex";
-  alternativesList.style.flexDirection = "column";
-  alternativesList.style.gap = "10px";
-
-  questionData.alternatives.forEach((alternative, index, alternatives) => {
-    const listItem = document.createElement("li");
-    listItem.classList.add("alternative-item");
-    listItem.style.padding = "0.6rem";
-
-    const label = document.createElement("label");
-    label.classList.add("alternative-label");
-
-    const radio = document.createElement("input");
-    radio.type = "radio";
-    radio.name = `question-${questionData.question_index}`;
-    radio.value = alternative.letter;
-    radio.id = `question-${questionData.question_index}-${alternative.letter}`;
-    console.log(alternative.selected);
-    radio.checked = alternative.selected;
-    radio.disabled = true;
-
-    if (alternative.is_correct) {
-      listItem.style.backgroundColor = "#BDEFBC";
-    }
-
-    if (alternative.selected && !alternative.is_correct) {
-      listItem.style.backgroundColor = "#F6C8C8";
-      questionInformation.style.backgroundColor = "#F6C8C8";
-    }
-
-    if (alternative.selected && alternative.is_correct) {
-      questionInformation.style.backgroundColor = "#BDEFBC";
-    }
-
-    label.appendChild(radio);
-    const letterSpan = document.createElement("span");
-    letterSpan.textContent = `${alternative.letter}: `;
-    label.appendChild(letterSpan);
-
-    if (alternative.text) {
-      const textSpan = document.createElement("span");
-      textSpan.textContent = alternative.text;
-      label.appendChild(textSpan);
-    }
-
-    if (alternative.file) {
-      const img = document.createElement("img");
-      img.src = alternative.file;
-      img.alt = `Alternative ${alternative.letter} Image`;
-      img.style.maxWidth = "100%";
-      img.style.height = "auto";
-      label.appendChild(img);
-    }
-
-    listItem.appendChild(label);
-    alternativesList.appendChild(listItem);
-  });
-
-  questionContent.appendChild(alternativesList);
-  questionContainer.appendChild(questionContent);
-
-  if (questionData.explanation) {
-    const titleParagraph = document.createElement("h3");
-    titleParagraph.textContent = "Explicação da Resposta:";
-    const explanationParagraph = document.createElement("p");
-    explanationParagraph.textContent = questionData.explanation;
-    questionContent.appendChild(titleParagraph);
-    questionContent.appendChild(explanationParagraph);
-  }
-
-  if (questionData.support_urls.length > 0) {
-    const titleParagraph = document.createElement("h3");
-    titleParagraph.textContent = "Links de suporte:";
-    const list = document.createElement("ul");
-    list.style.listStyleType = "none";
-    list.style.display = "flex";
-    list.style.flexDirection = "column";
-    list.style.padding = "0px";
-    list.style.gap = "10px";
-    questionData.support_urls.forEach((url) => {
-      const listItem = document.createElement("li");
-      const link = document.createElement("a");
-      link.href = url;
-      link.target = "_blank";
-      link.textContent = url;
-      listItem.appendChild(link);
-      list.appendChild(listItem);
-    });
-    questionContent.appendChild(titleParagraph);
-    questionContent.appendChild(list);
-  }
-
-  return questionContainer;
-}
-
-function createConfirmationModal(text, callback) {
-  const modalContainer = document.createElement("div");
-  modalContainer.style.position = "fixed";
-  modalContainer.style.top = "0";
-  modalContainer.style.left = "0";
-  modalContainer.style.width = "100%";
-  modalContainer.style.height = "100%";
-  modalContainer.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-  modalContainer.style.display = "flex";
-  modalContainer.style.justifyContent = "center";
-  modalContainer.style.alignItems = "center";
-  modalContainer.style.zIndex = "10";
-
-  const modalContent = document.createElement("div");
-  modalContent.style.backgroundColor = "white";
-  modalContent.style.padding = "20px";
-  modalContent.style.borderRadius = "5px";
-  modalContent.style.textAlign = "center";
-  modalContent.style.maxWidth = "80%";
-  modalContent.style.boxShadow = "0px 0px 10px rgba(0, 0, 0, 0.5)";
-
-  const textElement = document.createElement("p");
-  textElement.textContent = text;
-  textElement.style.marginBottom = "20px";
-
-  const buttonContainer = document.createElement("div");
-  buttonContainer.style.display = "flex";
-  buttonContainer.style.justifyContent = "center";
-  buttonContainer.style.gap = "20px";
-
-  const cancelButton = document.createElement("button");
-  cancelButton.textContent = "Cancelar";
-  cancelButton.style.backgroundColor = "#ccc";
-  cancelButton.style.color = "black";
-  cancelButton.style.border = "none";
-  cancelButton.style.padding = "10px 20px";
-  cancelButton.style.borderRadius = "5px";
-  cancelButton.style.cursor = "pointer";
-
-  const confirmButton = document.createElement("button");
-  confirmButton.textContent = "Confirmar";
-  confirmButton.style.backgroundColor = "#3D52A0";
-  confirmButton.style.color = "white";
-  confirmButton.style.border = "none";
-  confirmButton.style.padding = "10px 20px";
-  confirmButton.style.borderRadius = "5px";
-  confirmButton.style.cursor = "pointer";
-
-  buttonContainer.appendChild(cancelButton);
-  buttonContainer.appendChild(confirmButton);
-  modalContent.appendChild(textElement);
-  modalContent.appendChild(buttonContainer);
-  modalContainer.appendChild(modalContent);
-
-  function handleEscapeKey(event) {
-    if (event.key === "Escape") {
-      modalContainer.remove();
-      document.removeEventListener("keydown", handleEscapeKey);
-    }
-  }
-
-  document.addEventListener("keydown", handleEscapeKey);
-
-  modalContent.addEventListener("click", (event) => {
-    event.stopPropagation();
-  });
-
-  modalContainer.addEventListener("click", (event) => {
-    modalContainer.remove();
-    document.removeEventListener("keydown", handleEscapeKey);
-  });
-
-  cancelButton.addEventListener("click", () => {
-    modalContainer.remove();
-    document.removeEventListener("keydown", handleEscapeKey);
-  });
-
-  confirmButton.addEventListener("click", () => {
-    callback();
-    modalContainer.remove();
-    document.removeEventListener("keydown", handleEscapeKey);
-  });
-
-  return modalContainer;
-}
 
 export default element;
