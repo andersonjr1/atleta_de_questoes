343279aa01c5a29c9e8ab5e662ef80442c019101
Vinicius Tineli Paiva <vinicius.tinelipaiva@gmail.com>
2025-03-27 17:48:45 -0300

    fix: fix const router in answerRoutes

--- src/routes/answerRoutes.js
+++ src/routes/answerRoutes.js
@@ -1,4 +1,4 @@
-const router = require("express").Router;
+const router = require("express").Router();
 const { answerController } = require("../controllers/answerController.js");
 const { authToken } = require("../middlewares/authMiddleware.js");
 
