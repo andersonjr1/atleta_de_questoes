ba19e49c8a1631ce655e9664f913d835184fb62d
Anderson <andersonpaulojr1@gmail.com>
2025-03-27 15:47:04 -0300

    feat: add add question api route

--- src/repositories/questionRepository.js
+++ src/repositories/questionRepository.js
@@ -1,35 +1,105 @@
 const { pool } = require("../config/db.js");
 
 const questionRepository = {
-    getById: async (id) => {
-        try {
-            const result = await pool.query("SELECT * FROM questions WHERE id = $1", [id]);
-            if (result.rowCount === 0) {
-                throw new Error("Questão não encontrada");
-            }
-            return result.rows[0];
-        } catch (error) {
-            throw error;
-        }
-    },
-    delete: async (id) => {
-        try {
-            const result = await pool.query(
-                "DELETE FROM questions WHERE id = $1 RETURNING *",
-                [id]
-            );
+  getById: async (id) => {
+    try {
+      const result = await pool.query("SELECT * FROM questions WHERE id = $1", [
+        id,
+      ]);
+      if (result.rowCount === 0) {
+        throw new Error("Questão não encontrada");
+      }
+      return result.rows[0];
+    } catch (error) {
+      throw error;
+    }
+  },
+  delete: async (id) => {
+    try {
+      const result = await pool.query(
+        "DELETE FROM questions WHERE id = $1 RETURNING *",
+        [id]
+      );
 
-            if (result.rowCount === 0) {
-                throw Object.assign(new Error("Questão não encontrada"), {
-                    status: 404,
-                });
-            }
+      if (result.rowCount === 0) {
+        throw Object.assign(new Error("Questão não encontrada"), {
+          status: 404,
+        });
+      }
 
-            return { message: "Questão deletada com sucesso" };
-        } catch (error) {
-            throw error;
+      return { message: "Questão deletada com sucesso" };
+    } catch (error) {
+      throw error;
+    }
+  },
+  create: async (question) => {
+    const client = await pool.connect();
+    try {
+      await client.query("BEGIN");
+      const response = await client.query(
+        "INSERT INTO questions (vestibular, year, question_index, language, context, alternative_introduction, explanation, discipline, sub_discipline, level) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *",
+        [
+          question.vestibular,
+          question.year,
+          question.question_index,
+          question.language,
+          question.context,
+          question.alternative_introduction,
+          question.explanation,
+          question.discipline,
+          question.sub_discipline,
+          question.level,
+        ]
+      );
+
+      const questionId = await response.rows[0].id;
+
+      if (question.alternatives) {
+        for (const alternative of question.alternatives) {
+          const response = await client.query(
+            "INSERT INTO question_alternatives (id_question, letter, alternative_text, file_url, is_correct) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+            [
+              questionId,
+              alternative.letter,
+              alternative.text,
+              alternative.file_url,
+              alternative.is_correct,
+            ]
+          );
         }
-    },
+      } else {
+        const error = new Error("Questão sem alternativas");
+        error.status = 400;
+        throw error;
+      }
+
+      if (question.question_files) {
+        for (const file of question.question_files) {
+          const response = await client.query(
+            "INSERT INTO question_files (id_question, file_url) VALUES ($1, $2) RETURNING *",
+            [questionId, file]
+          );
+        }
+      }
+
+      if (question.support_urls) {
+        for (const supportLink of question.support_urls) {
+          const response = await client.query(
+            "INSERT INTO question_support (id_question, support_url) VALUES ($1, $2) RETURNING *",
+            [questionId, supportLink]
+          );
+        }
+      }
+
+      await client.query("COMMIT");
+      return response.rows[0];
+    } catch (error) {
+      await client.query("ROLLBACK");
+      throw error;
+    } finally {
+      client.release();
+    }
+  },
 };
 
 module.exports = { questionRepository };
--- src/routes/questionRoutes.js
+++ src/routes/questionRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { questionController } = require("../controllers/questionController.js");
 
+router.post("/questions", questionController.create);
 router.get("/questions/:id", questionController.getById);
 router.delete("/questions/:id", questionController.delete);
 
