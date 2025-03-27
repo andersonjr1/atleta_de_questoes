594e1a330df7c5921c12d86604fc1778cbfba9c0
Anderson <andersonpaulojr1@gmail.com>
2025-03-27 08:31:51 -0300

    fix: change how loading of the page works

--- src/public/pages/authorized-user/script.js
+++ src/public/pages/authorized-user/script.js
@@ -5,13 +5,12 @@ const app = document.getElementById("app");
 const link = document.createElement("link");
 document.head.appendChild(link);
 const pathname = window.location.pathname;
+link.rel = "stylesheet";
 
 if (pathname == "/inicio" || pathname == "/inicio/") {
-  app.appendChild(elementWelcome);
-  link.rel = "stylesheet";
   link.href = "/pages/authorized-user/welcome/welcome.css";
+  app.appendChild(elementWelcome);
 } else if (pathname == "/simulado" || pathname == "/simulado/") {
   link.href = "/pages/authorized-user/exam/exam.css";
-  link.rel = "stylesheet";
   app.appendChild(elementExam);
 }
