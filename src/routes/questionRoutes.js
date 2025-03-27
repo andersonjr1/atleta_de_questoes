de2eaea9beeaddb341a4f110b7835a4bcaaa8bd3
Matheus Bispo Machtura <math.machtura@gmail.com>
2025-03-26 23:00:38 -0300

    feat: add to question router with search action

--- src/routes/questionRoutes.js
+++ src/routes/questionRoutes.js
@@ -3,5 +3,7 @@ const { questionController } = require("../controllers/questionController.js");
 
 router.get("/questions/:id", questionController.getById);
 router.delete("/questions/:id", questionController.delete);
+router.get("/search", questionController.search);
+
 
 module.exports = router;
