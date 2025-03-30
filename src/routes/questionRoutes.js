eba48a8c6323e22b7f41b160de6e70677c6c9375
Matheus Bispo Machtura <math.machtura@gmail.com>
2025-03-30 11:13:35 -0300

    fix: fix question route and repository

--- src/routes/questionRoutes.js
+++ src/routes/questionRoutes.js
@@ -1,9 +1,8 @@
 const router = require("express").Router();
 const { questionController } = require("../controllers/questionController.js");
 
+router.get("/questions/search", questionController.search);
 router.get("/questions/:id", questionController.getById);
 router.delete("/questions/:id", questionController.delete);
-router.get("/search", questionController.search);
-
 
 module.exports = router;
