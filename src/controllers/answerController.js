81c2d2387540ce365726e285f02e1441a70e8ce2
Anderson <andersonpaulojr1@gmail.com>
2025-04-01 13:25:41 -0300

    feat: save answer logic

--- src/controllers/answerController.js
+++ src/controllers/answerController.js
@@ -2,53 +2,55 @@ const { answerService } = require("../services");
 const { authToken } = require("../middlewares/authMiddleware.js");
 
 const answerController = {
-    saveAnswer: async (req, res) => {
-        try {
-            const { questionId, alternativeId, isExam } = req.body;
-            const accountId = req.user.id;
+  saveNormalAnswer: async (req, res) => {
+    try {
+      const { questionId, alternativeId } = req.body;
+      const accountId = req.user.id;
 
-            const answer = await answerService.saveAnswer({
-                accountId,
-                questionId,
-                alternativeId,
-                isExam: isExam || false
-            });
+      const answer = await answerService.saveNormalAnswer({
+        accountId,
+        questionId,
+        alternativeId,
+      });
 
-            res.status(201).json(answer);
-        } catch (error) {
-            const statusCode = error.status || 500;
-            res.status(statusCode).json({ message: error.message });
-        }
-    },
-
-    getUserAnswers: async (req, res) => {
-        try {
-            const accountId = req.user.id;
-            const answers = await answerService.getUserAnswers(accountId);
-            res.status(200).json(answers);
-        } catch (error) {
-            const statusCode = error.status || 500;
-            res.status(statusCode).json({ message: error.message });
-        }
-    },
-
-    getSpecificAnswer: async (req, res) => {
-        try {
-            const accountId = req.user.id;
-            const { questionId } = req.params;
-
-            const answer = await answerService.getSpecificAnswer(accountId, questionId);
-
-            if (!answer) {
-                return res.status(404).json({ message: "Resposta não encontrada" });
-            }
-            
-            res.status(200).json(answer);
-        } catch (error) {
-            const statusCode = error.status || 500;
-            res.status(statusCode).json({ message: error.message });
-        }
+      res.status(201).json(answer);
+    } catch (error) {
+      const statusCode = error.status || 500;
+      res.status(statusCode).json({ message: error.message });
     }
+  },
+
+  getUserAnswers: async (req, res) => {
+    try {
+      const accountId = req.user.id;
+      const answers = await answerService.getUserAnswers(accountId);
+      res.status(200).json(answers);
+    } catch (error) {
+      const statusCode = error.status || 500;
+      res.status(statusCode).json({ message: error.message });
+    }
+  },
+
+  getSpecificAnswer: async (req, res) => {
+    try {
+      const accountId = req.user.id;
+      const { questionId } = req.params;
+
+      const answer = await answerService.getSpecificAnswer(
+        accountId,
+        questionId
+      );
+
+      if (!answer) {
+        return res.status(404).json({ message: "Resposta não encontrada" });
+      }
+
+      res.status(200).json(answer);
+    } catch (error) {
+      const statusCode = error.status || 500;
+      res.status(statusCode).json({ message: error.message });
+    }
+  },
 };
 
 module.exports = { answerController };
