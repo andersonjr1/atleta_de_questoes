19d6728727e89c8853f147a3c4812039ae79f9e5
Vinicius Tineli Paiva <vinicius.tinelipaiva@gmail.com>
2025-04-01 15:00:25 -0300

    feat: integrate API fetch in main.js

--- src/public/pages/leaderboard-page/main.js
+++ src/public/pages/leaderboard-page/main.js
@@ -1,12 +1,50 @@
 import { renderHeader } from "./views/header.js";
 import { renderLeaderboard } from "./views/leaderboard.js";
 import { renderFooter } from "./views/footer.js";
+import messageCreate from "../../components/message.js";
+import { getLoggedUserId } from "../../utils/authUtils.js";
 
-function initApp() {
+async function fetchLeaderboard() {
+    try {
+        const response = await fetch('/api/answers/leaderboard');
+
+        if (!response.ok) {
+            throw new Error(`Erro: ${response.status}`);
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error("Falha ao buscar ranking:", error);
+        document.body.appendChild(
+            messageCreate(false, "Falha ao corregar o ranking")
+        );
+        return[];
+    }
+}
+
+async function initApp() {
     const app = document.getElementById("app");
 
     app.appendChild(renderHeader());
-    app.appendChild(renderLeaderboard());
+   
+    const loadingDiv = document.createElement("div");
+    loadingDiv.className = "loading-state";
+    loadingDiv.innerHTML = `
+        <div class="spinner"><div>
+        <p>Carregando ranking...<p>
+    `;
+    app.appendChild(loadingDiv);
+
+    try {
+        const leaderboardData = await fetchLeaderboard();
+
+        app.remove(loadingDiv);
+        app.appendChild(renderLeaderboard(leaderboardData));
+    } catch (error) {
+        app.removeChild(loadingDiv);
+        app.appendChild(renderLeaderboard());
+    } 
+
     app.appendChild(renderFooter());
 }
 
