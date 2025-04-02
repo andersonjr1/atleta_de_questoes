112d8d42d640ec4f8f6ea06c5f9e4a8a7d2174d3
Anderson <andersonpaulojr1@gmail.com>
2025-04-02 12:28:38 -0300

    feat: add middleware to route

--- src/routes/examRoutes.js
+++ src/routes/examRoutes.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const { examController } = require("../controllers/examController.js");
+const { openExam } = require("../middlewares/openExam.js");
 const { authToken } = require("../middlewares/authMiddleware.js");
 
 router.get("/exam", authToken, examController.getAllExams);
 router.get("/exam/:examId", authToken, examController.getExamById);
-router.post("/exam", authToken, examController.createExam);
+router.post("/exam", authToken, openExam, examController.createExam);
 router.put("/exam/:examId", authToken, examController.respondExam);
 router.put(
   "/exam/:examId/question/:questionId",
