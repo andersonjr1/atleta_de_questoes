2f2736a740a9fe4d6a7e22cea02681e9eaccad41
Anderson <andersonpaulojr1@gmail.com>
2025-04-03 14:02:44 -0300

    style: the footer was not in the bottom of the page

--- src/public/pages/authorized-user/leaderboard-page/leaderboard-page.js
+++ src/public/pages/authorized-user/leaderboard-page/leaderboard-page.js
@@ -4,9 +4,15 @@ import Footer from "/components/footer.js";
 
 function leaderboardPage() {
   const element = document.createElement("div");
+  const leaderboardContainer = document.createElement("div");
 
+  element.style.display = "flex";
+  element.style.height = "100vh";
+  element.style.flexDirection = "column";
+  leaderboardContainer.style.flexGrow = 2;
   element.appendChild(Header());
-  element.appendChild(renderLeaderboard());
+  element.appendChild(leaderboardContainer);
+  leaderboardContainer.appendChild(renderLeaderboard());
   element.appendChild(Footer());
 
   return element;
