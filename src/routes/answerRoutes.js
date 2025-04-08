7655dde043e2c799cadd5a2d9ff0dd20b67f8129
Anderson <andersonpaulojr1@gmail.com>
2025-04-08 14:34:44 -0300

    refactor: remove unused code

--- src/routes/answerRoutes.js
+++ src/routes/answerRoutes.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 const { answerController } = require("../controllers");
-const { authToken } = require("../middlewares/authMiddleware.js");
 
 router.post("/", answerController.saveNormalAnswer);
 router.get("/", answerController.getUserAnswers);
