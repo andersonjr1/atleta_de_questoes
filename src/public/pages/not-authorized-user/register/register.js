1b3c7d0b647d164f8f2d453311d4a1e74fea7f40
Anderson <andersonpaulojr1@gmail.com>
2025-04-09 11:05:05 -0300

    fix: front-end work in any server.
    Remove "localhost:port" from fetch request, so that it can work in any server.

--- src/public/pages/not-authorized-user/register/register.js
+++ src/public/pages/not-authorized-user/register/register.js
@@ -81,7 +81,7 @@ function RegisterPage() {
         return;
       }
 
-      const response = await fetch("http://localhost:4000/api/register", {
+      const response = await fetch("/api/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
